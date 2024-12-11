0a0ab2b914d9cc3ad4b60d903c9cf94d8e298b5c
sravella <sravella@osius.com>
2024-12-11 11:36:50 +0530

    Adding rounting and reinstall packages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'registration', component: RegistrationComponent },
+    { path: 'home', component: HomeComponent },
+];
