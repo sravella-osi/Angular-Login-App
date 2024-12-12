d313d8e8c2d7e809e8fd930facd843c7d73fe46c
sravella <sravella@osius.com>
2024-12-12 12:25:08 +0530

    Fixed RouterLinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
